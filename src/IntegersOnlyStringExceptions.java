4d87345c61a600b5d7db4f2d3719743b2d85df29
Aleksandar Hristov <alexanderhristov93@abv.bg>
2016-07-12 17:30:23 +0300

    test commit

--- src/IntegersOnlyStringExceptions.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by clouway on 22.06.16.
- */
-public class IntegersOnlyStringExceptions extends Exception {
-    public IntegersOnlyStringExceptions() {
-        super("\ndsadasda");
-    }
-}
--- src/Sumator.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by clouway on 22.06.16.
- */
-public class Sumator {
-    public String sum(String str1, String str2) {
-        return Integer.toString(Integer.parseInt(str1) + Integer.parseInt(str2));
-    }
-}
