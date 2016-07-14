f7982d4ae6a726be634f15fd37f6458977ffeaf8
Aleksandar Hristov <alexanderhristov93@abv.bg>
2016-07-14 10:42:51 +0300

    fixed all comments i think

--- src/com/clouway/endpointfilter/Endpoint.java
+++ src/com/clouway/endpointfilter/Endpoint.java
@@ -4,6 +4,12 @@ package com.clouway.endpointfilter;
  * @author Aleksandar Hristov <alexanderhristov93@gmail.com>
  */
 
+/**
+ * Comparison the url.
+ *@param url the first number to sum
+ *@return url is matches or not matches (true or false)(boolean)
+ */
+
 public interface Endpoint {
     boolean matches(String url);
 }
